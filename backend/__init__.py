a704e3227ebd74b6b95dbbfcf2f41ac9d2d06037
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-02-08 20:57:19 +0000

    cors

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-import pymongo
-
+from flask_cors import CORS, cross_origin()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    cors = CORS(app)
+
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.config['mongo_db_url'] = 'mongodb+srv://rafihamza2020:k2vWNEwgPfJXCIkZ@cluster0.bohiq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     app.config['JWT_SECRET_KEY'] = "alkjdlkasjdf;lkajhfa"
