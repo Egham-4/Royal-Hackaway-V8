39d230f17e4b4535d79a51939f4c9c868fc0bf0c
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-02-08 21:35:13 +0000

    fixing cors issues

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS, cross_origin()
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
