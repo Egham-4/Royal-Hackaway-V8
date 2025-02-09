34432d51f1b5436e644da170a357e6b516b1bad3
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-02-09 03:27:44 +0000

    created helper fetch function with authentication

--- frontend/src/app/utils/auth.tsx
+++ frontend/src/app/utils/auth.tsx
@@ -32,3 +32,13 @@ export function getUser() {
   const data = localStorage.getItem("user")
   return data ? JSON.parse(data) : null
 }
+
+export async function fetch_auth(url: string, opts) {
+  opts.headers = {
+    "Authorization": "Bearer " + getToken()
+  }
+  let response = await fetch(url, opts)
+  let json = await response.json()
+
+  return json
+}
