2e7e4be536b7ed7d436f43b8aa02ffabd077bb39
Kendrick@KhantThatLwin <Khant.Lwin.2023@live.rhul.ac.uk>
2025-02-09 06:52:54 +0000

    add button on projects page.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -36,9 +36,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
-      <Footer />
     </html>
   );
 }
