88563cbf65ddae769274a1a927c15632ca724c57
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-02-09 02:25:24 +0000

    changed backend port 5000-5001

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    API_URL: 'http://localhost:5000'
+    API_URL: 'http://localhost:5001'
   }
 };
 
